50202180e1db3f9948b6607a2fe4b9ead9fce0bc
nm <noelmiranda19@gmail.com>
2019-04-02 10:12:15 -1100

    Updates

---
+++ precache-manifest.f4085c7d6f7c841c5c28788840e51622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef3ee70a0d1f62d02c7",
+    "url": "/markdown/static/js/runtime~main.7d448bfc.js"
+  },
+  {
+    "revision": "d2bbdb69b957bc992c4b",
+    "url": "/markdown/static/js/main.ff8afe6f.chunk.js"
+  },
+  {
+    "revision": "f19a074c7775fb413e09",
+    "url": "/markdown/static/js/2.45d4cb91.chunk.js"
+  },
+  {
+    "revision": "d2bbdb69b957bc992c4b",
+    "url": "/markdown/static/css/main.45b3af02.chunk.css"
+  },
+  {
+    "revision": "f19a074c7775fb413e09",
+    "url": "/markdown/static/css/2.e80059dd.chunk.css"
+  },
+  {
+    "revision": "ab8368903d32a13d095d9761e5dde141",
+    "url": "/markdown/index.html"
+  }
+];
